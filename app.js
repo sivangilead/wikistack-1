380ad8380e65ac0b8265aa8ec238200aa1cae2eb
Bronwyn Harris <bronwyn.harris@Bronwyns-MacBook-Air.local>
2018-06-15 14:25:52 -0400

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const morgan = require('morgan');
 const app = express();
 const layout = require('./views/layout');
 const path = require('path');
+const { db } = require('./models');
+const models = require('./models');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './stylesheets')));
@@ -13,6 +20,12 @@ app.get('/', (req, res, next) => {
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+const init = async () => {
+    await models.db.sync({force: true})
+    
+    app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}!`);
-});
+    }); 
+}
+
+init();
