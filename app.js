62f5901387b0076ccf3154a4f3fa71e17bd5b470
Esther Park <estherspark91@gmail.com>
2018-06-15 11:55:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const layout = require('./views/layout');
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './stylesheets')));
+
+app.get('/', (req, res, next) => {
+    res.send(layout(''));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('hello world!');
+});
